b696c3cf8b7c542197f2cbf33bdbdf66cbbdcef4
Baptiste Noleau <bnoleau@adlere.fr>
2024-09-30 18:09:14 +0200

    wip

---
+++ run.sh
@@ -0,0 +1,11 @@
+
+db2rest_url=https://api-db2rest.apps.app1.numerique-interieur.com
+
+curl --request POST \
+--url ${db2rest_url}/v1/rdbms/pgdb/products \
+--header 'Content-Type: application/json' \
+--header 'User-Agent: insomnia/9.2.0' \
+--data '{
+    "name": "Big egg",
+    "extra_info": { "expiry_date": "2025-12-31", "exportable": true }
+}'
